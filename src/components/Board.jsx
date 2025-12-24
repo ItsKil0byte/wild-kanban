7af66b81ef338c8ce7389f99f9c17770cd20b94b
Alexander Lebedev <itskilobyte@gmail.com>
2025-12-25 00:56:08 +0500

    remove unused objects

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,11 @@
 import Column from "./Column";
 import NewColumnForm from "./NewColumnForm";
-import NewTaskForm from "./NewTaskForm";
 
 export default function Board({ board, dispatch }) {
   return (
     <div className="board">
       {board.columns.map((column) => (
-        <Column
-          key={column.id}
-          column={column}
-          dispatch={dispatch}
-          isFirst={column.id === 1}
-          isLast={column.id === board.columns.length}
-        />
+        <Column key={column.id} column={column} dispatch={dispatch} />
       ))}
       <NewColumnForm dispatch={dispatch} />
     </div>
